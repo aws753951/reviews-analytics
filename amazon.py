e2ed0ae2487efffd3650536d421fc5c667be335d
MarcZhu <a0983204766@gmail.com>
2022-03-08 17:37:33 +0800

    cal_avg_tea_ans

--- amazon.py
+++ amazon.py
@@ -1,14 +1,12 @@
 data = []
 count = 0
-total = 0
-avg = 0
 with open('reviews.txt','r') as f:
 	for line in f:
 		count += 1
 		data.append(line)
-		total += len(data[count-1])
 
-avg = total / count
+total = 0
+for d in data:
+	total += len(d)
 
-print(avg)
-
+print('平均是', total/len(data),'字數')
