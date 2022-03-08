44aad84a909b996bde5c05e3984d0a57213e9999
MarcZhu <a0983204766@gmail.com>
2022-03-08 20:45:21 +0800

    add list comprehension

--- amazon.py
+++ amazon.py
@@ -20,9 +20,18 @@ print('一共有 ', len(new), '筆留言小於100字')
 print(new[0])
 print(new[1])
 
-good = []
-for d in data:
-	if 'good' in d:
-		good.append(d)
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
 
-print('一共有', len(good), '筆提到 good')
+# print('一共有', len(good), '筆提到 good')
+
+#list comprehension
+
+good = [d for d in data if 'good' in d] # 第一個d為要放入good list的元素，可更換
+print(good[100])
+
+bad = ['bad' in d for d in data] # 為布林值，是否有bad值在d字串元素裡面，有則True
+print(bad[100])
+ 
