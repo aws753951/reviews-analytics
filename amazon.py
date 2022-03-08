879e3842cc9c06dd0a8dc8f590a3ffad23d9dced
MarcZhu <a0983204766@gmail.com>
2022-03-08 17:33:38 +0800

    cal_avg_my_ans

--- amazon.py
+++ amazon.py
@@ -1,9 +1,14 @@
 data = []
 count = 0
+total = 0
+avg = 0
 with open('reviews.txt','r') as f:
 	for line in f:
 		count += 1
 		data.append(line)
-		if count % 100000 == 0:
-			print(data[count-1])
+		total += len(data[count-1])
+
+avg = total / count
+
+print(avg)
 
